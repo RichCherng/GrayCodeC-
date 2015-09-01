bd9c704456610ef23c75b7b44ea6700459366d98
Rich Cherngchaosil <richer_po@hotmail.com>
2015-09-01 13:23:32 -0700

    Add writing to file function

--- GrayCode/GrayCode.cpp
+++ GrayCode/GrayCode.cpp
@@ -1,17 +1,27 @@
 #include <iostream>
 #include <bitset>
+#include <iostream>
+#include <fstream>
 
 using namespace::std;
 
 int main() {
    
-   const int N = 17;
+   const int N = 18;
    long int num = pow(2, N);
    int gray;
+   ofstream myfile;
+   string filename = to_string(N)+ "bit.txt";
+   
+   myfile.open(filename);
+
+
    for (int i = 0; i < num; i++) {
+      cout << i << endl;
       gray = i ^ (i / 2);
       bitset<N> bit(gray);
-      cout << bit << endl;
+      myfile << bit << endl;
    }
+   myfile.close();
    system("pause");
 }
